376819091c2ee154be973ef1b63a438e35ec13ea
locoplaya666 <93893581+locoplaya666@users.noreply.github.com>
2023-03-24 03:57:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,112 +1,38 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+// Variables
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
-\f0\fs24 \cf0 /* Estilos generales */\
-\
-body \{\
-  font-family: Arial, sans-serif;\
-  font-size: 16px;\
-  line-height: 1.6;\
-  color: #333;\
-\}\
-\
-a \{\
-  color: #007bff;\
-\}\
-\
-/* Encabezado */\
-\
-header \{\
-  background-color: #f8f8f8;\
-  padding: 20px 0;\
-  text-align: center;\
-\}\
-\
-header h1 \{\
-  margin: 0;\
-  font-size: 36px;\
-  font-weight: bold;\
-  color: #333;\
-\}\
-\
-header h2 \{\
-  margin: 10px 0 0;\
-  font-size: 24px;\
-  font-weight: normal;\
-  color: #888;\
-\}\
-\
-/* Navegaci\'f3n */\
-\
-nav \{\
-  background-color: #333;\
-  color: #fff;\
-  padding: 10px;\
-  text-align: center;\
-\}\
-\
-nav ul \{\
-  list-style: none;\
-  margin: 0;\
-  padding: 0;\
-\}\
-\
-nav li \{\
-  display: inline-block;\
-  margin-right: 10px;\
-\}\
-\
-nav a \{\
-  color: #fff;\
-  text-decoration: none;\
-  font-weight: bold;\
-  padding: 10px;\
-  transition: background-color 0.3s ease;\
-\}\
-\
-nav a:hover \{\
-  background-color: #007bff;\
-\}\
-\
-/* Secciones */\
-\
-section \{\
-  padding: 50px 0;\
-  text-align: center;\
-\}\
-\
-section h2 \{\
-  font-size: 36px;\
-  font-weight: bold;\
-  color: #333;\
-\}\
-\
-section p \{\
-  margin: 20px 0;\
-  font-size: 18px;\
-  line-height: 1.5;\
-  color: #888;\
-\}\
-\
-/* Pie de p\'e1gina */\
-\
-footer \{\
-  background-color: #333;\
-  color: #fff;\
-  padding: 20px 0;\
-  text-align: center;\
-\}\
-\
-footer p \{\
-  margin: 0;\
-  font-size: 14px;\
-\}\
-\
-footer a \{\
-  color: #fff;\
-\}\
+// Navigation Menu Toggle
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+// Close Navigation Menu on Link Click
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
+
+// Scroll to Top Button
+const scrollTopBtn = document.querySelector('.scroll-top');
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// Show Scroll to Top Button on Scroll
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const screenHeight = window.innerHeight;
+    if (scrollHeight > screenHeight) {
+        scrollTopBtn.classList.add('show-scroll');
+    } else {
+        scrollTopBtn.classList.remove('show-scroll');
+    }
+});
+
 }
